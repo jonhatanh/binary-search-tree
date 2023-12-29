c933d22dcb0c3533540fddb44e8924774d6a051c
jonhatanh <jonhatan.higuera@gmail.com>
2023-12-28 22:04:49 -0600

    Override toString in Node class

--- Node.js
+++ Node.js
@@ -1,13 +1,21 @@
 export default class Node {
-    #value = null;
-    left = null;
-    right = null;
+  #value = null;
+  left = null;
+  right = null;
 
-    constructor(value) {
-        this.#value = value;
-    }
+  constructor(value) {
+    this.#value = value;
+  }
 
-    get value() {
-        return this.#value;
-    }
+  get value() {
+    return this.#value;
+  }
+
+  toString() {
+    return `
+Node value: ${this.#value}
+Left value: ${this.left?.value ?? null}
+Right value: ${this.right?.value ?? null}
+        `;
+  }
 }
